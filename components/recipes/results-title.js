ac9756b840b7c924fba178d1ca5a38454992a50f
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-05-04 19:56:44 -0300

    Adding search results title logic

--- components/recipes/results-title.js
+++ components/recipes/results-title.js
@@ -3,11 +3,16 @@ import classes from './results-title.module.css';
 
 function ResultsTitle(props) {
 	const { difficulty, mealType } = props;
+	const titleCaseMealType =
+		mealType.charAt(0).toUpperCase() + mealType.slice(1);
+	const titleCaseDifficulty =
+		difficulty.charAt(0).toUpperCase() + difficulty.slice(1);
 
 	return (
 		<section className={classes.title}>
 			<h1>
-				{mealType} {difficulty} recipes{' '}
+				{titleCaseMealType !== 'All' && titleCaseMealType}{' '}
+				{titleCaseDifficulty !== 'All' && titleCaseDifficulty} Recipes:{' '}
 			</h1>
 			<Button link="/recipes">Show all recipes</Button>
 		</section>
