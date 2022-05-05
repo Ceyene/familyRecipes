4d511a5bf2b01dd84d2b5009143c7170541af513
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-05-05 17:38:03 -0300

    Working on the All Recipes page

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -1,13 +1,13 @@
 import { Fragment } from 'react';
 import { useRouter } from 'next/router';
 
-import { getAllRecipes } from '../../dummy-data';
+import { getAllRecipes } from '../../helpers/api-util';
 import RecipeList from '../../components/recipes/recipe-list';
 import RecipesSearch from '../../components/recipes/recipes-search';
 
-function AllRecipesPage() {
+function AllRecipesPage(props) {
 	const router = useRouter();
-	const recipes = getAllRecipes();
+	const { recipes } = props;
 
 	function findRecipesHandler(mealType, difficulty) {
 		const fullPath = `/recipes/${mealType}/${difficulty}`;
@@ -21,6 +21,17 @@ function AllRecipesPage() {
 			<RecipeList items={recipes} />
 		</Fragment>
 	);
+}
+
+export async function getStaticProps() {
+	const recipes = await getAllRecipes();
+
+	return {
+		props: {
+			recipes: recipes,
+		},
+		revalidate: 60, //every minute it regenerates request
+	};
 }
 
 export default AllRecipesPage;
