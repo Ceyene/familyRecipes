88df1c22d619c9d60f69c40aa8bc27dd434417f2
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-05-06 12:01:17 -0300

    Adding general meta tags and title to the head

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,12 @@ function MyApp({ Component, pageProps }) {
 	return (
 		<Layout>
 			<Head>
+				<meta charSet="utf-8" />
+				<title>Family Recipes - by Cyn Romero</title>
+				<meta
+					name="description"
+					content="A list of the traditional and not so traditional recipes I know"
+				/>
 				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</Head>
