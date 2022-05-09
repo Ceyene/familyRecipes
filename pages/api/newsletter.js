6377ecd63d3adc7b161b31a784e3b7283cadac56
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-05-09 12:53:55 -0300

    Connecting newsletter API Route to MongoDB

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,17 +1,38 @@
-function handler(req, res) {
-	//checking request method
-	if (req.method === 'POST') {
-		//extracting data from req body
-		const userEmail = req.body.email;
-		//checking validity of email (important, always check in the backend)
-		if (!userEmail || !userEmail.includes('@')) {
-			res.status(422).json({ message: 'Invalid email address' });
-			return;
-		}
+//dependencies
+import { MongoClient } from 'mongodb';
 
-		//storing in database and sending json response
-		console.log(userEmail);
-		res.status(201).json({ message: 'Signed Up!' });
+async function handler(req, res) {
+	try {
+		//checking request method
+		if (req.method === 'POST') {
+			//extracting data from req body
+			const userEmail = req.body.email;
+			//checking validity of email (important, always check in the backend)
+			if (!userEmail || !userEmail.includes('@')) {
+				res.status(422).json({ message: 'Invalid email address' });
+				return;
+			}
+
+			//USING MONGODB
+			//creating mongo client
+			const client = await MongoClient.connect(
+				`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.q2qeb.mongodb.net/newsletter?retryWrites=true&w=majority`
+			);
+			//creating db and collection
+			const db = client.db();
+			const recipesCollection = db.collection('emails');
+			//creating a document
+			const result = await recipesCollection.insertOne({ email: userEmail });
+			console.log(result);
+			//closing db connection
+			client.close();
+
+			//once stored in database, sending json response
+			res.status(201).json({ message: 'Signed Up!' });
+		}
+	} catch (error) {
+		console.log(error.message);
+		res.status(400).json({ message: error.message });
 	}
 }
 
