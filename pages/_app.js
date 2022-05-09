835843d978333817d05fdcb23141d76c28e6d76b
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-05-09 15:52:46 -0300

    Adding Notification Context and Provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,30 @@
 import Head from 'next/head';
 import Layout from '../components/layout/layout';
+import Notification from '../components/ui/notification';
+import { NotificationContextProvider } from '../store/notification-context';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Head>
-				<meta charSet="utf-8" />
-				<title>Family Recipes - by Cyn Romero</title>
-				<meta
-					name="description"
-					content="A list of the traditional and not so traditional recipes I know"
-				/>
-				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			</Head>
-			<Component {...pageProps} />
-		</Layout>
+		<NotificationContextProvider>
+			<Layout>
+				<Head>
+					<meta charSet="utf-8" />
+					<title>Family Recipes - by Cyn Romero</title>
+					<meta
+						name="description"
+						content="A list of the traditional and not so traditional recipes I know"
+					/>
+					<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+					<meta
+						name="viewport"
+						content="width=device-width, initial-scale=1.0"
+					/>
+				</Head>
+				<Component {...pageProps} />
+				<Notification title="Test" message="This is a test" status="pending" />
+			</Layout>
+		</NotificationContextProvider>
 	);
 }
 
