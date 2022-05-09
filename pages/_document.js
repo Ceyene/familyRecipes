1a2087352abab69d44aa2524f700968e22a1da98
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-05-09 20:03:26 -0300

    Added google fonts correctly

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,13 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang="en">
-				<Head />
+				<Head>
+					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+					<link
+						href="https://fonts.googleapis.com/css2?family=Bitter:wght@300&family=Roboto+Slab:wght@300;400&family=Send+Flowers&display=swap"
+						rel="stylesheet"
+					></link>
+				</Head>
 				<body>
 					<div id="overlays" />
 					<Main />
