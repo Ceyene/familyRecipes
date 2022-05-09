e986af55f4cfb8955c0b2a3ad5359ecba04bac69
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-05-09 13:29:49 -0300

    Fetching comments list from db

--- pages/api/comments/[recipeId].js
+++ pages/api/comments/[recipeId].js
@@ -5,6 +5,11 @@ import { MongoClient } from 'mongodb';
 async function handler(req, res) {
 	try {
 		const recipeId = req.query.recipeId; //getting id value from path
+		//USING MONGODB
+		//creating mongo client
+		const client = await MongoClient.connect(
+			`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.q2qeb.mongodb.net/recipes?retryWrites=true&w=majority`
+		);
 		//checking req method
 		if (req.method === 'POST') {
 			//adding server-side validation (can't be tricked from client-side)
@@ -29,29 +34,32 @@ async function handler(req, res) {
 				id: recipeId,
 			};
 
-			//USING MONGODB
-			//creating mongo client
-			const client = await MongoClient.connect(
-				`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.q2qeb.mongodb.net/recipes?retryWrites=true&w=majority`
-			);
 			//creating db and collection
 			const db = client.db();
 			const recipesCollection = db.collection('comments');
 			//creating a document
 			const result = await recipesCollection.insertOne(newComment);
 			newComment.id = result.insertedId;
-			//closing db connection
-			client.close();
+
 			//sending json response to client
 			res.status(201).json({ message: 'Added comment!' });
 		}
 		if (req.method === 'GET') {
-			const dummyList = [
-				{ id: 'c1', name: 'Marcos', text: 'Awesome recipe, thanks' },
-				{ id: 'c2', name: 'Cyn', text: 'Great recipe, thanks' },
-			];
-			res.status(200).json({ comments: dummyList });
+			//USING MONGODB
+			//creating db and collection
+			const db = client.db();
+			const recipesCollection = db.collection('comments');
+			//fetching all documents in this collection for this recipe id, in descending order, as an array
+			const documents = await recipesCollection
+				.find()
+				.sort({ _id: -1 })
+				.toArray();
+			//returning to the client
+			res.status(200).json({ comments: documents });
 		}
+
+		//closing db connection
+		client.close();
 	} catch (error) {
 		console.log(error);
 	}
